54a51b59d2a8ef40f1897c74f7e2cfecb64e7e7d
17crown1901mituru <17crown1901@gmail.com>
2026-03-28 03:04:41 +0900

    DaemonApi.kt の更新

--- app/src/main/java/com/mwvscript/daemon/DaemonApi.kt
+++ app/src/main/java/com/mwvscript/daemon/DaemonApi.kt
@@ -1,1 +1,22 @@
+package your.package.name.daemon
 
+interface DaemonController {
+    fun start(port: Int = 5555)
+    fun stop()
+    fun sendToClient(data: ByteArray)
+}
+
+interface ShellExecutor {
+    fun exec(cmd: String): ExecResult
+}
+
+data class ExecResult(
+    val stdout: String,
+    val stderr: String,
+    val exitCode: Int
+)
+
+interface SessionState {
+    fun set(key: String, value: Any?)
+    fun get(key: String): Any?
+}
